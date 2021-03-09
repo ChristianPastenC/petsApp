75b4c4175c6e597ab9c0d6099c43f460607215e1
ChristianPC <pastencristian2@gmail.com>
2021-03-09 14:15:53 -0600

    Automatizar slider

--- src/app/dashboard-components/carousel/carousel.component.ts
+++ src/app/dashboard-components/carousel/carousel.component.ts
@@ -1,19 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, transition, style, animate } from "@angular/animations";
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
+  animations: [
+    trigger('carouselAnimation', [
+      transition('void => *', [
+        style({ opacity: 0 }),
+        animate('2000ms', style({ opacity: 1 }))
+      ]),
+      transition('* => void', [
+        animate('2000ms', style({ opacity: 0 }))
+      ])
+    ])
+  ]
 })
 export class CarouselComponent implements OnInit {
 
+  id;
+  
   constructor() { }
 
   ngOnInit(): void {
-    //this.playIntervalo();  
+    this.onNextClick();
+    this.id = setInterval(() => {
+      this.onNextClick(); 
+    }, 4000);
   }
 
-  slides = [
+  public slides = [
     {src: "assets/img1.jpg"},
     {src: "assets/img2.jpg"},
     {src: "assets/img3.jpg"},
@@ -25,7 +42,8 @@ export class CarouselComponent implements OnInit {
 
   onPreviousClick() {
     const previous = this.currentSlide - 1;
-    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;  
+    this.currentSlide = previous < 0 ? this.slides.length - 1 : previous;
+    
   }
 
   onNextClick() {
@@ -33,5 +51,5 @@ export class CarouselComponent implements OnInit {
     this.currentSlide = next === this.slides.length ? 0 : next;
     console.log("next clicked, new current slide is: ", this.currentSlide);
   }
-  
+    
 }
