2e044151dd91c0e4e1eed710b30c33182e5bb649
ChristianPC <pastencristian2@gmail.com>
2021-04-14 13:40:36 -0500

    Promover componente de Da en Adopcion

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { VerIndexComponent } from '../ver-index/ver-index.component';
 import { InfoComponent } from '../info/info.component';
+import { DarAdopcionComponent} from '../dar-adopcion/dar-adopcion.component';
 
 const routes: Routes = [
   { path: '',
       component: HomeComponent,
       children: [
         { path: '', component: VerIndexComponent},
-        { path: 'info', component:InfoComponent}
+        { path: 'info', component:InfoComponent},
+        { path: 'daenadopcion', component: DarAdopcionComponent}
       ]
     }
   ];
